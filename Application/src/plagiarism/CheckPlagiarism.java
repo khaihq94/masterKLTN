31be2b8bf4872563b80bd9c3e06aaeaa4e787c82
khaihq94 <hwk94@yahoo.com.vn>
2016-11-25 01:12:28 +0700

    Hoàn thành tính độ tương tự giữa 2 văn bản
    + Hoàn thành tính độ tương tự dựa trên thứ tự từ
    + Hoàn thành hàm tính độ tương tự getSimSO dựa trên ngữ nghĩa và thứ tự
    từ (Trọng số cho ngữ nghĩa là 0.6, thứ tự từ là 0.4)

--- Application/src/plagiarism/CheckPlagiarism.java
+++ Application/src/plagiarism/CheckPlagiarism.java
@@ -1,6 +1,7 @@
 package plagiarism;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.io.*;
 
 public class CheckPlagiarism {
@@ -217,13 +218,14 @@ public class CheckPlagiarism {
 	 * 
 	 * */
 	public ArrayList<String> seperateWords(String text){
+		text = text.toLowerCase();
 		//Khai báo mảng words để chứa các từ trong văn bản
 		ArrayList<String> words = new ArrayList<>();
 		//Khai báo mảng stopWordsList để đọc file chứa các từ dừng
 		ArrayList<String> stopWords = readStopWordsFile("vietnamese-stopwords.txt");
 		
 		// Loại bỏ các dấu đặc biệt
-		String content = text.replaceAll("[-^=+\\\\|\\[{\\]};:'\",<>/#()]*", "").replaceAll("\u2026", "");
+		String content = text.replaceAll("[-^=+\\\\|\\[{\\]};:'\".,<>/#()]*", "").replaceAll("\u2026", "");
 		
 		// biến k duyệt qua từng ký tự trong văn bản
 		int k = 0;
@@ -244,17 +246,17 @@ public class CheckPlagiarism {
 					continue;
 				}
 				word = content.substring(position, k).trim();
-				//Kiểm tra xem từ đó có phải là stopword ko
+				/*//Kiểm tra xem từ đó có phải là stopword ko
 				for(String stopWord : stopWords){
 					//Nếu là stopword, gán bằng rỗng, thoát khỏi vòng lặp kiểm tra
 					if(word.equals(stopWord)){
 						word = "";
 						break;
 					}
-				}
+				}*/
 				// Nếu từ khác rỗng thì thêm vào mảng
 				if(!word.equals("")){
-					words.add(word);
+					words.add(word.trim());
 				}
 				// Tăng k để duyệt ký tự kế tiếp
 				k++;
@@ -263,6 +265,18 @@ public class CheckPlagiarism {
 			} else {
 				k++;
 			}
+		}
+		//Loại bỏ stopword khỏi mảng
+		for(String stopWord : stopWords){
+			//Nếu là stopword, xóa khỏi mảng
+			for(int i = 0; i < words.size(); i ++){
+				if(words.contains(stopWord)){
+					words.remove(stopWord);
+				} else{
+					break;
+				}
+			}
+			
 		}
 		return words;
 	}
@@ -348,5 +362,125 @@ public class CheckPlagiarism {
 		
 		return (sim1 + sim2)/2;
 	}
-
+	
+	/*
+	 * Lấy ra tập từ phân biệt của 2 văn bản
+	 * input: 2 văn bản đã được tokenize
+	 * output: mảng chứa tập từ phân biệt
+	 * */
+	public ArrayList<String> getListDiffWords(ArrayList<String> content1, ArrayList<String> content2){
+		ArrayList<String> listDiffWords = new ArrayList<>();
+		for(String word1 : content1){
+			//Nếu mảng có từ này rồi thì bỏ qua không kiểm tra
+			if(listDiffWords.contains(word1)){
+				break;
+			}
+			//Kiểm tra từ của content1 có trong content2 hay ko, nếu có thì thêm từ vào tập từ phân biệt và xóa từ đó khỏi content2
+			for(int i = 0; i < content2.size(); i++){
+				if(content2.contains(word1)){
+					if(!listDiffWords.contains(word1)){
+						listDiffWords.add(word1);
+					}
+					content2.remove(word1);
+				} else {
+					break;
+				}
+			}
+			
+		}
+		for(String word2 : content2){
+			//Nếu mảng có từ này rồi thì bỏ qua không kiểm tra
+			if(listDiffWords.contains(word2)){
+				break;
+			}
+			//Kiểm tra từ của content2 có trong content1 hay ko, nếu có thì thêm từ vào tập từ phân biệt và xóa từ đó khỏi content1
+			for(int i = 0; i < content1.size(); i++){
+				if(content1.contains(word2)){
+					if(!listDiffWords.contains(word2)){
+						listDiffWords.add(word2);
+					}
+					content1.remove(word2);
+				}
+			}
+			
+		}
+		return listDiffWords;
+	}
+	
+	/*
+	 * Lấy vectơ đặc trưng theo thứ tự từ
+	 * input: mảng chứa tập từ phân biệt của 2 văn bản, mảng chứa từ của văn bản cần tính vectơ
+	 * output: mảng chứa vectơ đặc trưng
+	 * */
+	public ArrayList<ArrayList<Integer>> getVecto(ArrayList<String> listDiffWord, ArrayList<String> content){
+		//Khai báo mảng vecto R
+		ArrayList<ArrayList<Integer>> R = new ArrayList<ArrayList<Integer>>();
+		for(int i = 0; i < listDiffWord.size(); i++){
+			//Khai báo mảng tạm tmp để lưu trữ vị trí của 1 từ
+			ArrayList<Integer> tmp = new ArrayList<>();
+			//Lấy từ thứ i trong mảng listDiffWord
+			String diffWord = listDiffWord.get(i).trim();
+			//Duyệt qua mảng content để tìm thứ tự của diffWord trong mảng
+			for(int j = 0; j < content.size(); j++){
+				String word = content.get(j).trim();
+				//Nếu tìm thấy diffWord trong content thì lưu vị trí vào mảng tmp
+				if(diffWord.equalsIgnoreCase(word)){
+					tmp.add(j);
+				}
+			}
+			//Sau khi duyệt xong và tìm ra thứ tự của 1 từ trong Tập từ phân biệt thì lưu vào mảng vecto R
+			R.add(tmp);
+		}
+		return R;
+	}
+	
+	
+	/*
+	 * Tính độ tương tự theo thứ tự từ
+	 * input: 2 mảng là 2 vecto của 2 văn bản cần so sánh
+	 * output: simO - độ tương tự về thứ tự từ của 2 văn bản
+	 * */
+	public double getSimO(String content1, String content2){
+		double numerator, denominator, simO;
+		int i = 0;
+		double tmpNum = 0;
+		double tmpDen = 0;
+		ArrayList<String> words1 = seperateWords(content1);
+		ArrayList<String> words2 = seperateWords(content2);
+		ArrayList<String> T = getListDiffWords(words1, words2);
+		
+		ArrayList<ArrayList<Integer>> R1 = getVecto(T, seperateWords(content1));
+		ArrayList<ArrayList<Integer>> R2 = getVecto(T, seperateWords(content2));
+		//Tính numerator và denominator trước khi lấy căn
+		while (i < R1.size() && i < R2.size()) {
+			int j = 0;
+			int maxSize;
+			if (R1.get(i).size() < R2.get(i).size()) {
+				maxSize = R1.get(i).size();
+			} else {
+				maxSize = R2.get(i).size();
+			}
+			while (j < maxSize) {
+				tmpNum = tmpNum + (R1.get(i).get(j) - R2.get(i).get(j)) * (R1.get(i).get(j) - R2.get(i).get(j));
+				tmpDen = tmpDen + (R1.get(i).get(j) + R2.get(i).get(j)) * (R1.get(i).get(j) + R2.get(i).get(j));
+				j++;
+			}
+			i++;
+		}
+		numerator = Math.sqrt(tmpNum);
+		denominator = Math.sqrt(tmpDen);
+		simO = 1 - (numerator / denominator);
+		return simO;
+	}
+	
+	/*
+	 * Tính độ tương tự giữa 2 văn bản
+	 * input: 2 văn bản đã đc tách từ
+	 * output: độ tương tự đc tính dựa trên Độ tương tự ngữ nghĩa và Độ tương tự thứ tự từ
+	 * */
+	public double getSimSO(String content1, String content2){
+		double simS = getSimS(content1, content2);
+		double simO = getSimO(content1, content2);
+		return simS * 0.6 + simO * 0.4;
+	}
 }
